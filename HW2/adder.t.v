a287be621376283be59c823038b28875903f8887
Adam Selker <adamselker@gmail.com>
2018-09-16 16:04:29 -0400

    Fix adder.t.v, and add adder

--- HW2/adder.t.v
+++ HW2/adder.t.v
@@ -6,7 +6,8 @@ module testFullAdder();
     reg a, b, cIn;
     wire sum, cOut;
 
-    behavioralFullAdder adder (sum, cOut, a, b, cIn);
+    //behavioralFullAdder adder (sum, cOut, a, b, cIn);
+		structuralFullAdder adder(sum, cOut, a, b, cIn);
 
     initial begin
 			$display("a b cIn | sum cOut | expected sum cOut");
@@ -17,11 +18,11 @@ module testFullAdder();
 			a = 0; b = 1; cIn = 0; #1000
 			$display("%b %b  %b  |  %b   %b   |           1   0", a, b, cIn, sum, cOut);
 			a = 0; b = 1; cIn = 1; #1000
-			$display("%b %b  %b  |  %b   %b   |           1   1", a, b, cIn, sum, cOut);
+			$display("%b %b  %b  |  %b   %b   |           0   1", a, b, cIn, sum, cOut);
 			a = 1; b = 0; cIn = 0; #1000
-			$display("%b %b  %b  |  %b   %b   |           0   0", a, b, cIn, sum, cOut);
+			$display("%b %b  %b  |  %b   %b   |           1   0", a, b, cIn, sum, cOut);
 			a = 1; b = 0; cIn = 1; #1000
-			$display("%b %b  %b  |  %b   %b   |           1   1", a, b, cIn, sum, cOut);
+			$display("%b %b  %b  |  %b   %b   |           0   1", a, b, cIn, sum, cOut);
 			a = 1; b = 1; cIn = 0; #1000
 			$display("%b %b  %b  |  %b   %b   |           0   1", a, b, cIn, sum, cOut);
 			a = 1; b = 1; cIn = 1; #1000
--- HW2/adder.v
+++ HW2/adder.v
@@ -18,7 +18,21 @@ module structuralFullAdder
     output carryout,
     input a, 
     input b, 
-    input carryin
+    input c
 );
-    // Your adder code here
+
+	wire axb, axbxc, ab, bc, ac, abc, abubc; 
+
+	xor axb_(axb, a, b);
+	xor axbxc_(axbxc, axb, c);
+
+	and ab_(ab, a, b);
+	and ac_(ac, a, c);
+	and bc_(bc, b, c);
+	and abc_(abc, ab, c);
+
+	or sum_(sum, axbxc, abc);
+
+	or abubc_(abubc, ab, bc);
+	or carryout_(carryout, abubc, ac);
 endmodule
