8a5feced03d09255907946c5a348a21cb3613c47
Adam Selker <adamselker@gmail.com>
2018-09-16 15:45:04 -0400

    Add decoder

--- HW2/decoder.v
+++ HW2/decoder.v
@@ -10,7 +10,6 @@ module behavioralDecoder
     assign {out3,out2,out1,out0}=enable<<{address1,address0};
 endmodule
 
-
 module structuralDecoder
 (
     output o0, o1, o2, o3,
@@ -21,10 +20,17 @@ module structuralDecoder
 	wire na0, na1;
 	wire a0int, a1int, a2int, a3int;
 
-	not n_a0(na0, a0)
-	not n_a1(na1, a1)
+	not n_a0(na0, a0);
+	not n_a1(na1, a1);
 	
-	and o0(o0int, na0, na1)
-	and o0
-endmodule
+	and o0_and(o0int, na0, na1);
+	and o1_and(o1int, a0, na1);
+	and o2_and(o2int, na0, a1);
+	and o3_and(o3int, a0, a1);
 
+	and o0_and2(o0, o0int, e);
+	and o1_and2(o1, o1int, e);
+	and o2_and2(o2, o2int, e);
+	and o3_and2(o3, o3int, e);
+
+endmodule
